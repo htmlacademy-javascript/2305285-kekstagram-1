239a204607a888bdac94d2644a5d623442dbe4de
CrashStatic <thecrashstatic@gmail.com>
2024-02-21 21:41:38 +0300

    Работает с функцией проверки палиндрома

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+// Функция для проверки, является ли строка палиндромом
+
+const checkPalindrom = (palindrom) => {
+
+  let revercePalindrom = palindrom.toLowerCase().split('').reverse().join('');
+  return palindrom.toLowerCase().replaceAll(' ', '') === revercePalindrom.replaceAll(' ', '');
+};
+
+
+// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
+
+const getInteger = (string) => {
+  let newString = string.match(/\d+/g).join('');
+  return newString ? parseInt(newString, 10) : NaN;
+};
+
+// let str = 'Здесь нет цифр!'; // Момент напряжения…
+// let result = str.match(/\d+/); // Может, ложное срабатывание?
+// result = result ? parseInt(result[0], 10) : 'Цифры не найдены';
+
+
+// Функция, увеличивающая строку до заданной длины
+const getRequiredLength = (originalString, minLength, extensionString) => {
+
+}
