8088f2ced81ff55f419f01b7f6abe2c6e965b1eb
CrashStatic <thecrashstatic@gmail.com>
2024-03-21 18:33:59 +0300

    Вносит изменения в функцию рендера миниматюр

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import './data.js';
 import { generatePhotos } from './data.js';
-import './miniatures.js';
 import { renderMiniatures } from './miniatures.js';
 
 const photos = generatePhotos();
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,19 +1,20 @@
 const containerElement = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const fragment = document.createDocumentFragment();
-
 const renderMiniatures = (photos) => {
+  const fragment = document.createDocumentFragment();
+
   photos.forEach(({ url, description, likes, comments }) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    const pictureImageElement = pictureElement.querySelector('.picture__img');
-    pictureImageElement.src = url;
-    pictureImageElement.alt = description;
+    const imageElement = pictureElement.querySelector('.picture__img');
+    imageElement.src = url;
+    imageElement.alt = description;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
     fragment.appendChild(pictureElement);
-    containerElement.appendChild(fragment);
   });
+
+  containerElement.appendChild(fragment);
 };
 
 export { renderMiniatures };
