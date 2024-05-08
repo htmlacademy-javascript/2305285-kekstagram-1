7ab4911089ccb8d1b1c6c641708bddc53e2fbe92
CrashStatic <thecrashstatic@gmail.com>
2024-05-08 18:48:33 +0300

    Работает с сортировкой фотографий

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ import './scale.js';
 import './filters.js';
 import { getData } from './api.js';
 import { renderGallery } from './gallery.js';
+import { showImgFilters, sortPhotosByComments, getDefaultPhotos, sortPhotosByRandom, getRandomPhotos, getDiscussedPhotos } from './sort.js';
+
+const COMMENTS_SIZE = 10;
 
 getData()
   .then((photos) => {
     renderGallery(photos);
+    showImgFilters();
+    getDefaultPhotos(() => renderGallery(photos));
+    getRandomPhotos(() => renderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)));
+    getDiscussedPhotos(() => renderGallery(photos.slice().sort(sortPhotosByComments)));
   }).catch((err) => err);
+
+// getData()
+//   .then((photos) => {
+//     renderGallery(photos);
+//     showImgFilters();
+//   }).catch((err) => err);
---
+++ js/sort.js
@@ -0,0 +1,44 @@
+const imgFiltersContainer = document.querySelector('.img-filters');
+const filterDefaultButton = document.querySelector('#filter-default');
+const filterRandomButton = document.querySelector('#filter-random');
+const filterDiscussedButton = document.querySelector('#filter-discussed');
+
+const containerElement = document.querySelector('.pictures');
+// const pictureElement = document.querySelector('.picture');
+
+const showImgFilters = () => {
+  imgFiltersContainer.classList.remove('img-filters--inactive');
+};
+
+const sortPhotosByComments = (photoA, photoB) => photoB.comments.length - photoA.comments.length;
+
+const sortPhotosByRandom = () => Math.random() - 0.5;
+
+const getDefaultPhotos = (cb) => {
+
+  filterDefaultButton.addEventListener('click', () => {
+    containerElement.innerHTML = '';
+    filterRandomButton.classList.toggle('img-filters__button--active');
+    cb();
+  });
+};
+
+const getRandomPhotos = (cb) => {
+
+  filterRandomButton.addEventListener('click', () => {
+    containerElement.innerHTML = '';
+    filterRandomButton.classList.toggle('img-filters__button--active');
+    cb();
+  });
+};
+
+const getDiscussedPhotos = (cb) => {
+
+  filterDiscussedButton.addEventListener('click', () => {
+    containerElement.innerHTML = '';
+    filterDiscussedButton.classList.toggle('img-filters__button--active');
+    cb();
+  });
+};
+
+export { showImgFilters, sortPhotosByComments, sortPhotosByRandom, getDefaultPhotos, getRandomPhotos, getDiscussedPhotos };
