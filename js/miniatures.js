93ab381b0ed44bbe0fd4979fa71c07f9c2d6e920
CrashStatic <thecrashstatic@gmail.com>
2024-05-09 14:35:03 +0300

    Изменяет модуль отрисосовки миниатюр

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,21 +1,51 @@
+// const containerElement = document.querySelector('.pictures');
+// const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+// const renderMiniatures = (photos) => {
+//   const fragment = document.createDocumentFragment();
+
+//   photos.forEach(({ url, description, likes, comments, id }) => {
+//     const pictureElement = pictureTemplate.cloneNode(true);
+//     const imageElement = pictureElement.querySelector('.picture__img');
+//     imageElement.src = url;
+//     imageElement.alt = description;
+//     pictureElement.querySelector('.picture__likes').textContent = likes;
+//     pictureElement.querySelector('.picture__comments').textContent = comments.length;
+//     pictureElement.dataset.thumbnailId = id;
+//     fragment.appendChild(pictureElement);
+//   });
+
+//   containerElement.appendChild(fragment);
+// };
+
+// export { renderMiniatures, containerElement };
+
+
 const containerElement = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderMiniatures = (photos) => {
-  const fragment = document.createDocumentFragment();
+const renderThumbnail = ({ url, description, likes, comments, id }) => {
 
-  photos.forEach(({ url, description, likes, comments, id }) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    const imageElement = pictureElement.querySelector('.picture__img');
-    imageElement.src = url;
-    imageElement.alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureElement.dataset.thumbnailId = id;
-    fragment.appendChild(pictureElement);
+  const thumbnail = pictureTemplate.cloneNode(true);
+  const imageElement = thumbnail.querySelector('.picture__img');
+  imageElement.src = url;
+  imageElement.alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.dataset.thumbnailId = id;
+
+  return thumbnail;
+};
+
+const renderMiniatures = (photos, container) => {
+  container.querySelectorAll('.picture').forEach((element) => element.remove());
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    const thumbnail = renderThumbnail(photo);
+    fragment.appendChild(thumbnail);
   });
 
-  containerElement.appendChild(fragment);
+  container.appendChild(fragment);
 };
 
 export { renderMiniatures, containerElement };
--- js/sort.js
+++ js/sort.js
@@ -1,10 +1,15 @@
+// import { containerElement } from './miniatures.js';
+
 const imgFiltersContainer = document.querySelector('.img-filters');
 const filterDefaultButton = document.querySelector('#filter-default');
 const filterRandomButton = document.querySelector('#filter-random');
 const filterDiscussedButton = document.querySelector('#filter-discussed');
 
-const containerElement = document.querySelector('.pictures');
+// const containerElement = document.querySelector('.pictures');
 // const pictureElement = document.querySelector('.picture');
+
+// let picturesContainer = [];
+
 
 const showImgFilters = () => {
   imgFiltersContainer.classList.remove('img-filters--inactive');
@@ -17,7 +22,7 @@ const sortPhotosByRandom = () => Math.random() - 0.5;
 const getDefaultPhotos = (cb) => {
 
   filterDefaultButton.addEventListener('click', () => {
-    containerElement.innerHTML = '';
+    // containerElement.removeChild();
     filterRandomButton.classList.toggle('img-filters__button--active');
     cb();
   });
@@ -26,7 +31,7 @@ const getDefaultPhotos = (cb) => {
 const getRandomPhotos = (cb) => {
 
   filterRandomButton.addEventListener('click', () => {
-    containerElement.innerHTML = '';
+    // containerElement.innerHTML = '';
     filterRandomButton.classList.toggle('img-filters__button--active');
     cb();
   });
@@ -35,7 +40,7 @@ const getRandomPhotos = (cb) => {
 const getDiscussedPhotos = (cb) => {
 
   filterDiscussedButton.addEventListener('click', () => {
-    containerElement.innerHTML = '';
+    // containerElement.innerHTML = '';
     filterDiscussedButton.classList.toggle('img-filters__button--active');
     cb();
   });
