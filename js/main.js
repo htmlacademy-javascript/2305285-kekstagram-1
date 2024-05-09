096a30c1dbd460b2d548c0eb376257ece75319bc
CrashStatic <thecrashstatic@gmail.com>
2024-05-09 15:00:25 +0300

    Работает с функцие устранения дребезга

--- js/main.js
+++ js/main.js
@@ -4,20 +4,30 @@ import './filters.js';
 import { getData } from './api.js';
 import { renderGallery } from './gallery.js';
 import { showImgFilters, sortPhotosByComments, getDefaultPhotos, sortPhotosByRandom, getRandomPhotos, getDiscussedPhotos } from './sort.js';
-
+import { debounce } from './util.js';
 const COMMENTS_SIZE = 10;
 
 getData()
   .then((photos) => {
     renderGallery(photos);
+    const debounceRenderGallery = debounce(renderGallery);
     showImgFilters();
-    getDefaultPhotos(() => renderGallery(photos));
-    getRandomPhotos(() => renderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)));
-    getDiscussedPhotos(() => renderGallery(photos.slice().sort(sortPhotosByComments)));
+    getDefaultPhotos(() => debounceRenderGallery(photos));
+    getRandomPhotos(() => debounceRenderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)));
+    getDiscussedPhotos(() => debounceRenderGallery(photos.slice().sort(sortPhotosByComments)));
   }).catch((err) => err);
 
 // getData()
 //   .then((photos) => {
 //     renderGallery(photos);
 //     showImgFilters();
+//   }).catch((err) => err);
+
+// getData()
+//   .then((photos) => {
+//     renderGallery(photos);
+//     showImgFilters();
+//     getDefaultPhotos(() => renderGallery(photos));
+//     getRandomPhotos(() => renderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)));
+//     getDiscussedPhotos(() => renderGallery(photos.slice().sort(sortPhotosByComments)));
 //   }).catch((err) => err);
--- js/util.js
+++ js/util.js
@@ -21,4 +21,25 @@ const createIdGenerator = () => {
 // Проверка нажатия клавиши Escape
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export { getRandomPositiveInteger, getRandomArrayElement, createIdGenerator, isEscapeKey };
+// Функция debounce для устранения дребезга
+
+const debounce = (callback, timeoutDelay = 500) => {
+
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+};
+
+export { getRandomPositiveInteger, getRandomArrayElement, createIdGenerator, isEscapeKey, debounce };
