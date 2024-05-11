62708a37503c392c9e87ddf8954369f41534b37b
CrashStatic <thecrashstatic@gmail.com>
2024-05-11 16:50:26 +0300

    Чистит модули по критериям

--- js/util.js
+++ js/util.js
@@ -1,25 +1,5 @@
 const RERENDER_DELAY = 500;
 
-// Функция получения целого числа из переданного диапазона
-
-const getRandomPositiveInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-  return function () {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-};
-
 // Проверка нажатия клавиши Escape
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -44,4 +24,4 @@ const debounce = (callback, timeoutDelay = RERENDER_DELAY) => {
   };
 };
 
-export { getRandomPositiveInteger, getRandomArrayElement, createIdGenerator, isEscapeKey, debounce };
+export { isEscapeKey, debounce };
