fe4525a8adbe1c18c555c7a1fff73df6c6ae2074
CrashStatic <thecrashstatic@gmail.com>
2024-05-13 19:03:22 +0300

    Работает с дребезгом при фильтрации

--- js/main.js
+++ js/main.js
@@ -4,28 +4,9 @@ import './effects.js';
 import { getData } from './api.js';
 import { renderGallery } from './gallery.js';
 import { initFilters } from './filters.js';
-// import { debounce } from './util.js';
 
 getData()
   .then((photos) => {
     renderGallery(photos);
-    // debounce(initFilters(photos));
     initFilters(photos);
-    // const debounceRenderGallery = debounce(renderGallery);
   }).catch((err) => err);
-
-
-// showImgFilters();
-// getCurrentFilterPhotos(() => debounceRenderGallery(photos), filterDefaultButton);
-// getCurrentFilterPhotos(() => debounceRenderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)), filterRandomButton);
-// getCurrentFilterPhotos(() => debounceRenderGallery(photos.slice().sort(sortPhotosByComments)), filterDiscussedButton);
-
-
-// getData()
-//   .then((photos) => {
-//     renderGallery(photos);
-//     showImgFilters();
-//     getDefaultPhotos(() => renderGallery(photos));
-//     getRandomPhotos(() => renderGallery(photos.slice().sort(sortPhotosByRandom).slice(0, COMMENTS_SIZE)));
-//     getDiscussedPhotos(() => renderGallery(photos.slice().sort(sortPhotosByComments)));
-//   }).catch((err) => err);
