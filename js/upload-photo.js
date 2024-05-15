a576b98d49b253337c6c4672e28aad10fa5e752a
CrashStatic <thecrashstatic@gmail.com>
2024-05-15 14:07:22 +0300

    Дорабатывает модуль загрузки фотографии пользователя

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -3,6 +3,7 @@ import { imgPreviewElement } from './effects.js';
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const chooseFileElement = document.querySelector('#upload-file');
+const effectPreviewElements = document.querySelectorAll('.effects__preview');
 
 chooseFileElement.addEventListener('change', () => {
   const file = chooseFileElement.files[0];
@@ -12,5 +13,8 @@ chooseFileElement.addEventListener('change', () => {
 
   if (acceptableExtension) {
     imgPreviewElement.src = URL.createObjectURL(file);
+    effectPreviewElements.forEach((photo) => {
+      photo.style.backgroundImage = `url(${imgPreviewElement.src})`;
+    });
   }
 });
